8a16107ac830cfd046317ef89094811b991e6d34
Coda Bool <codabool@pm.me>
2021-08-10 23:34:04 -0400

    revert to loading image files

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,18 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|jpe?g)$/,
+        use: [
+          {
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images/"
+            },
+            loader: "file-loader"
+          }
+        ]
       }
     ]
   }
